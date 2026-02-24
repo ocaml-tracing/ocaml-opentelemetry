4e63b2a3a21bdd94ad6979b92854760ef6c40e59
Simon Cruanes <simon.cruanes.2007@m4x.org>
2026-02-24 16:02:36 -0500

    detail

--- src/lib/dynamic_enricher.ml
+++ src/lib/dynamic_enricher.ml
@@ -14,6 +14,9 @@ let collect () : Key_value.t list =
     (fun f ->
       match f () with
       | kvs -> acc := List.rev_append kvs !acc
-      | exception _ -> ())
+      | exception exn ->
+        let bt = Printexc.get_raw_backtrace () in
+        Printf.eprintf "opentelemetry: dynamic_enricher raised %s\n%s%!"
+          (Printexc.to_string exn) (Printexc.raw_backtrace_to_string bt))
     (Alist.get enrichers_);
   !acc
